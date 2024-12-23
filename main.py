9b5b721494a188fdd02a3281aff8df00f5c9d4c6
melanie2040 <melanie.choi@dconstruct.plus>
2024-12-23 14:12:09 +0800

    summary

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
 from facer import facer
+import numpy as np
 import matplotlib.pyplot as plt
 
 path_to_images = "./face-images"
-images = facer.load
+images = facer.load_images(path_to_images)
+
+# Detect landmarks for each face
+landmarks, faces = facer.detect_face_landmarks(images)
+
+# Use the detected landmarks to create an average face
+average_face = facer.create_average_face(faces, landmarks, save_image=True)
+print(type(average_face))
+average_face = average_face[0]
+
+
+if isinstance(average_face, np.ndarray):
+    plt.imshow(average_face)
+    plt.show()
+else:
+    print("The average face is not in a valid image format.")
